88eaf21c84c5a36e5e312d09477323917d24bda2
luncliff <luncliff@gmail.com>
2022-08-28 02:01:01 +0900

    test: rewrite test_main.cpp

--- test/test_mf_scheduler.cpp
+++ test/test_mf_scheduler.cpp
@@ -4,74 +4,17 @@
  */
 #include <CppUnitTest.h>
 
-#include <spdlog/pattern_formatter.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
-#include <spdlog/sinks/stdout_sinks.h>
-#include <spdlog/spdlog.h>
-
 #include <Mferror.h>
 #include <mfapi.h>
 #include <mfidl.h>
+#include <spdlog/spdlog.h>
 #include <winrt/windows.system.h>
 
 #include "mf_scheduler.hpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-/**
- * @brief Redirect spdlog messages to `Logger::WriteMessage`
- */
-class vstest_sink final : public spdlog::sinks::sink {
-    std::unique_ptr<spdlog::formatter> formatter;
-
-  public:
-    void log(const spdlog::details::log_msg& msg) override {
-        spdlog::memory_buf_t buf{};
-        formatter->format(msg, buf);
-        std::string txt = fmt::to_string(buf);
-        Logger::WriteMessage(txt.c_str());
-    }
-    void flush() override {
-        Logger::WriteMessage(L"\n");
-    }
-    void set_pattern(const std::string& p) override {
-        formatter = std::make_unique<spdlog::pattern_formatter>(p);
-    }
-    void set_formatter(std::unique_ptr<spdlog::formatter> f) override {
-        formatter = std::move(f);
-    }
-};
-
-auto make_logger(const char* name, FILE* fout) noexcept(false) {
-    spdlog::sink_ptr sink0 = [fout]() -> spdlog::sink_ptr {
-        using mutex_t = spdlog::details::console_nullmutex;
-        if (fout == stdout || fout == stderr)
-            return std::make_shared<spdlog::sinks::stdout_color_sink_st>();
-        using sink_t = spdlog::sinks::stdout_sink_base<mutex_t>;
-        return std::make_shared<sink_t>(fout);
-    }();
-    spdlog::sink_ptr sink1 = std::make_shared<vstest_sink>();
-    return std::make_shared<spdlog::logger>(name, spdlog::sinks_init_list{sink0, sink1});
-}
-
-TEST_MODULE_INITIALIZE(Initialize) {
-    // change default logger to use vstest_sink
-    auto logger = make_logger("test", stdout);
-    logger->set_pattern("%T.%e [%L] %8t %v");
-    logger->set_level(spdlog::level::level_enum::debug);
-    spdlog::set_default_logger(logger);
-    // Default is `multi_threaded`...
-    winrt::init_apartment(winrt::apartment_type::single_threaded);
-    winrt::check_hresult(MFStartup(MF_VERSION, MFSTARTUP_FULL));
-}
-
-TEST_MODULE_CLEANUP(Cleanup) {
-    MFShutdown();
-    winrt::uninit_apartment();
-}
-
-class mf_scheduler_test_case
-    : public ::Microsoft::VisualStudio::CppUnitTestFramework::TestClass<mf_scheduler_test_case> {
+class mf_scheduler_test_case : public TestClass<mf_scheduler_test_case> {
   private:
     std::unique_ptr<mf_scheduler_t> scheduler = nullptr;
 
@@ -92,7 +35,7 @@ class mf_scheduler_test_case
         scheduler = nullptr;
     }
 
-    TEST_METHOD(method0) {
+    TEST_METHOD(test_scheduler_handle) {
         std::lock_guard lck{*scheduler};
         Assert::AreNotEqual<DWORD>(0, scheduler->handle());
     }
